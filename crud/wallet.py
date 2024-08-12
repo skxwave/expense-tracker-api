25fc64f480288bb9d3e25ff6a55798286b68b754
Roman <cheepa30@gmail.com>
2024-08-12 18:38:02 +0300

    crud for wallet

---
+++ crud/wallet.py
@@ -0,0 +1,38 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from core.schemas.wallet import WalletCreate, WalletUpdate
+from core.models import Wallet
+
+
+async def show_all(
+    session: AsyncSession,
+):
+    result = await session.execute(select(Wallet))
+    return result.scalars().all()
+
+
+async def create(
+    wallet_create: WalletCreate,
+    session: AsyncSession,
+):
+    wallet = Wallet(**wallet_create.model_dump())
+    session.add(wallet)
+    await session.commit()
+    return wallet
+
+
+async def put(
+    wallet_update: WalletUpdate,
+    wallet: Wallet,
+    session: AsyncSession,
+):
+    pass
+
+
+async def delete(
+    wallet: Wallet,
+    session: AsyncSession,
+):
+    await session.delete(wallet)
+    await session.commit()
