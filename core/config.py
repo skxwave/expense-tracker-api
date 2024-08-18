8f33e00d280d7ec14204dcf09c7dabdd65456df5
Roman <cheepa30@gmail.com>
2024-08-18 14:42:36 +0300

    edit config

--- core/config.py
+++ core/config.py
@@ -2,6 +2,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import BaseModel, MySQLDsn
 
 
+class SuperUser(BaseModel):
+    email: str
+    password: str
+    is_active: bool = True
+    is_superuser: bool = True
+    is_verified: bool = True
+
+
 class Run(BaseModel):
     host: str = "localhost"
     port: int = 8000
@@ -14,8 +22,26 @@ class DatabaseSettings(BaseModel):
     max_overflow: int = 10
 
 
+class AccessToken(BaseModel):
+    lifetime_seconds: int = 3600
+    reset_password_token_secret: str
+    verification_token_secret: str
+
+
+class ApiV1Prefix(BaseModel):
+    prefix: str = "/v1"
+    auth: str = "/auth"
+
+
 class ApiPrefix(BaseModel):
-    pass
+    prefix: str = "/api"
+    v1: ApiV1Prefix = ApiV1Prefix()
+
+    @property
+    def bearer_token_url(self) -> str:
+        parts = (self.prefix, self.v1.prefix, self.v1.auth)
+        path = "".join(parts)
+        return path[1:]
 
 
 class Settings(BaseSettings):
@@ -25,8 +51,11 @@ class Settings(BaseSettings):
         env_nested_delimiter="__",
         case_sensitive=False,
     )
-    db: DatabaseSettings
+    api: ApiPrefix = ApiPrefix()
     run: Run = Run()
+    db: DatabaseSettings
+    access_token: AccessToken
+    superuser: SuperUser
 
 
 settings = Settings()
