506951ce70cb208cb3a351ce6a0be9d07a43df97
Roman <cheepa30@gmail.com>
2024-08-19 21:09:22 +0300

    dependency fix

--- api/dependencies/transaction.py
+++ api/dependencies/transaction.py
@@ -3,13 +3,15 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 from sqlalchemy import select
 
-from core.models import db, Transaction
+from core.models import db, Transaction, User
+from ..api_v1.fastapi_users_router import current_user
 
 
 async def find_transaction(
     transaction_type: str,
     session: AsyncSession,
     transaction_id: int,
+    user_id: int,
 ):
     stmt = (
         select(Transaction)
@@ -19,12 +21,13 @@ async def find_transaction(
         )
         .where(Transaction.transaction_type == transaction_type)
         .where(Transaction.id == transaction_id)
+        .where(Transaction.user_id == user_id)
     )
     transaction = await session.scalar(statement=stmt)
     if not transaction:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"{transaction_type} not found",
+            detail=f"{transaction_type.capitalize()} not found",
         )
     return transaction
 
@@ -32,8 +35,10 @@ async def find_transaction(
 async def find_expense(
     session: AsyncSession = Depends(db.session_getter),
     transaction_id: int = Path,
+    user: User = Depends(current_user),
 ):
     return await find_transaction(
+        user_id=user.id,
         transaction_type="expense",
         session=session,
         transaction_id=transaction_id,
@@ -43,8 +48,10 @@ async def find_expense(
 async def find_income(
     session: AsyncSession = Depends(db.session_getter),
     transaction_id: int = Path,
+    user: User = Depends(current_user),
 ):
     return await find_transaction(
+        user_id=user.id,
         transaction_type="income",
         session=session,
         transaction_id=transaction_id,
